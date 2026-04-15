4e9fafd2a0cc4f55c4e1e69e6427d187fd57b229
Swapnadeep Khuntia <123418613+SwapnadeepKhuntia@users.noreply.github.com>
2026-04-15 11:22:13 +0530

    first commit

---
+++ Backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.router.js";
+import messageRouter from "./routes/message.router.js";
+dotenv.config();
+
+
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+app.use("/api/auth",authRouter);
+app.use("/api/messages",messageRouter);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
