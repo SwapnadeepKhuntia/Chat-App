ecad3250d7f9d28e695f5b9cd0ff5a0760cf7cf3
Swapnadeep Khuntia <123418613+SwapnadeepKhuntia@users.noreply.github.com>
2026-04-23 19:40:04 +0530

    Login and logout Section Added

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -45,3 +45,38 @@ export const register = async(req, res) => {
     res.status(500).json({ message: "Error registering user", error: error.message });
   }
 }
+
+export const login = async(req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user && user.password === password) {
+      generateToken(user._id,res);  
+      res.json({
+        id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+        profilePicture: user.profilePicture,
+      });
+    }
+    else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } 
+  catch (error) {
+    res.status(500).json({ message: "Error logging in", error: error.message });
+  }
+}
+
+export const logout = async(req, res) => {
+  try { 
+    res.cookie("jwt","",{maxAge:0});
+    res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging out", error: error.message });
+  }
+}
