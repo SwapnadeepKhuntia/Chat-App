647e1b4a034d8684b870286e0cf11c3ca8237fcc
Swapnadeep Khuntia <123418613+SwapnadeepKhuntia@users.noreply.github.com>
2026-04-23 12:27:39 +0530

    Wellcome email implemented

---
+++ Backend/src/utils/resent.js
@@ -0,0 +1,10 @@
+import { Resend } from "resend";
+import "dotenv/config";
+
+
+export const resendClient = new Resend(process.env.RESENT_EMAIL_SECRET);
+
+export const sender =  {
+    email:process.env.EMAIL_FROM,
+    name:process.env.EMAIL_FROM_NAME
+}
