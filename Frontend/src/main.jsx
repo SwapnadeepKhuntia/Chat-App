2ea05aac71a78b83d9876723069b620280c80d25
Swapnadeep Khuntia <123418613+SwapnadeepKhuntia@users.noreply.github.com>
2026-05-27 23:54:05 +0530

    SignUp Page in Frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
+import {Toaster} from "react-hot-toast";
 import { store } from './store/store.js'
 import './index.css'
 import App from './App.jsx'
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter> 
      <Provider store={store}>
        <App />
+       <Toaster/>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
